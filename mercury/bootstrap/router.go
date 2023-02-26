a685eb8349691a229898916cfbcc94822d4a303e
guyu <lee.gaecoli@gmail.com>
2023-02-26 18:57:55 +0800

    add shutdown server func

--- mercury/bootstrap/router.go
+++ mercury/bootstrap/router.go
@@ -1,9 +1,16 @@
 package bootstrap
 
 import (
+	"context"
 	"github.com/gin-gonic/gin"
+	"log"
 	"mercury/mercury/global"
 	"mercury/mercury/routes"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func setupRouter() *gin.Engine {
@@ -17,6 +24,30 @@ func setupRouter() *gin.Engine {
 
 func RunServer() {
 	r := setupRouter()
-	r.Run(":" + global.App.Config.App.Port)
-}
 
+	server := &http.Server{
+		Addr:    ":" + global.App.Config.App.Port,
+		Handler: r,
+	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutdown server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatal("Server shutdown:", err)
+	}
+
+	log.Println("Server exiting")
+}
--- mercury/routes/api.go
+++ mercury/routes/api.go
@@ -3,10 +3,16 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"time"
 )
 
 func SetApiGroupRoutes(router *gin.RouterGroup) {
 	router.GET("ping", func(c *gin.Context) {
 		c.String(http.StatusOK, "pong")
+	})
+
+	router.GET("/test", func(c *gin.Context) {
+		time.Sleep(5 * time.Second)
+		c.String(http.StatusOK, "success")
 	})
 }
