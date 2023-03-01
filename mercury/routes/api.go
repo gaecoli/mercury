c3a576eea69da1d98b6e72f1acdb0294009f757d
guyu <lee.gaecoli@gmail.com>
2023-03-02 00:04:36 +0800

    feat: add user register mod[unfinished]
    # Conflicts:
    #	mercury/app/common/request/user.go

--- mercury/routes/api.go
+++ mercury/routes/api.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+	"mercury/mercury/app/common/request"
 	"net/http"
 	"time"
 )
@@ -14,5 +15,18 @@ func SetApiGroupRoutes(router *gin.RouterGroup) {
 	router.GET("/test", func(c *gin.Context) {
 		time.Sleep(5 * time.Second)
 		c.String(http.StatusOK, "success")
+	})
+
+	router.POST("/user/register", func(c *gin.Context) {
+		var form request.Register
+		if err := c.ShouldBindJSON(&form); err != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"error": request.GetErrorMsg(form, err),
+			})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"message": "success",
+		})
 	})
 }
