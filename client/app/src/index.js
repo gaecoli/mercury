1a449c3bacec5d1f58293bb7d50ed102de7f0aa4
Restyled.io <commits@restyled.io>
2023-03-13 08:54:12 +0000

    Restyled by prettier

--- client/app/src/index.js
+++ client/app/src/index.js
@@ -1,12 +1,12 @@
-import './index.css';
+import "./index.css";
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
